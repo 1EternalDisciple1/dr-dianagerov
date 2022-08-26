1cfccd3f1704b0a1474f8856b83e06ba9100e92c
Arsen <arsen@getmeva.com>
2022-08-26 13:33:09 +0500

    add robot.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,13 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{userAgent: '*', allow: '/'}]
+      },
+    },
+
     'gatsby-plugin-offline',
   ],
 }

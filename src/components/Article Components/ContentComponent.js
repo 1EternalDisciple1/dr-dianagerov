d8f0ff1ced33d3439e3f10adadb9e645f4bda65c
Arsen <arsen@getmeva.com>
2023-05-25 14:41:51 +0500

    add setting

--- src/components/Article Components/ContentComponent.js
+++ src/components/Article Components/ContentComponent.js
@@ -1,19 +1,13 @@
 import React from "react"
 import ReactMarkdown from "react-markdown"
 
-
-
-const ContentComponent = (props) => {
-
-  return(
+const ContentComponent = props => {
+  return (
     <article>
       <div className="data_article">{props.dataArticle}</div>
-      <ReactMarkdown
-      source={props.contentArticle}
-      />
+      <ReactMarkdown source={props.contentArticle} escapeHtml={false} />
     </article>
   )
 }
-
 
 export default ContentComponent
